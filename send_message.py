3d8dd2a2e97fa551502e9f729fdafbae3b10fd5b
wumanchung-pixel <wumanchung@gmail.com>
2025-09-28 16:15:02 +0800

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -2,9 +2,8 @@ import requests
 import os
 
 # 從 GitHub Secrets 讀取 Token / Chat ID
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-
+TOKEN = os.getenv("7107000163:AAFxhRaTHPwM0uk_W-X-FjyKekbXmvv3Pz0")
+CHAT_ID = os.getenv("1915326639")
 def send_message(text):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": text}
